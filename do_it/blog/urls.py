25d5ba13529bc089bf5289835346e856a339f3c4
YooSeonHo <tjsgh503@naver.com>
2021-08-02 15:58:22 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- do_it/blog/urls.py
+++ do_it/blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index),
-    path('<int:pk>/',views.single_post_page),
-    
+    # path('',views.index),
+    path('',views.PostList.as_view()),
+    # path('<int:pk>/',views.single_post_page),
+    path('<int:pk>/',views.PostDetail.as_view()),
 ]
