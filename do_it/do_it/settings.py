73f914f301892410a914b5f90b341f1f2819b9a5
Yoo SeonHo <tjsgh503@naver.com>
2022-08-26 00:50:46 +0900

    Apply .gitignore

--- do_it/do_it/settings.py
+++ do_it/do_it/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+SECRET_KEY = 'django-insecure-*l0_%(uw%hpeip&@l1$ux)i)2qbv=jfy93fo3zjwf#*&&^c&7_'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
